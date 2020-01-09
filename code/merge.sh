69b4afb408c002cc31635149050a4601581f3991
sofyalaski <44316816+sofyalaski@users.noreply.github.com>
2020-01-09 13:54:09 +0100

    Update merge.sh

--- code/merge.sh
+++ code/merge.sh
@@ -3,12 +3,12 @@
 #call from benchmark
 
 for D in *; do
-	if [[ ! -f "$D/Ensembl/merge_SJ_all.tsv" ]]; then
+	if [[ ! -f "$D/../../analysis/$D/rnaseq/merge_SJ_all.tsv" ]]; then
 		echo "no merged exons"
 	elif [[ ! -f "$D/thoraxe/s_exon_table.csv" ]]; then
 		echo "no s-exon table!"
 	else
 		echo "$D"
-		ipython /home/sofya/Documents/TranscriptAnnotation/rnaseq/code/merge2.py "$D/Ensembl/merge_SJ_all.tsv" "$D/thoraxe/s_exon_table.csv"
+		ipython /home/sofya/Documents/TranscriptAnnotation/rnaseq/code/merge2.py "$D/../../analysis/$D/rnaseq/merge_SJ_all.tsv" "$D/thoraxe/s_exon_table.csv"
 	fi
 done
